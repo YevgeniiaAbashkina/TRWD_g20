42945ffd43d4e9a2838e992004638488c540ef99
YevgeniiaAbashkina <abashkina_evg@ukr.net>
2021-09-18 12:05:22 +0200

    addit new folder

---
+++ 57_14-09__58_15-09/class-work/maket/src/components/Meals.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import Meal from "./Meal";
+import FoodApi from "../store/data";
+
+export default class Meals extends React.Component{
+    state={
+        loading: true,
+        meals:[],
+        error: null
+    }
+
+    /* constructor(props){
+        super(props)
+    } */
+
+    renderMeals(){
+        const {loading, meals, error}={...this.state}
+        return loading && !error ? <Loader/> : error ? <Error message={error}/> : 
+        meals.map(meal => <Meal key={meal.idMeal} meal={meal}/>)
+    }
+
+    componentDidMount(){
+        this.componentChangeCountry(this.props.country)
+        console.log("kuku")
+    }
+
+    componentDidUpdate(prevProps){
+        if(prevProps.country !== this.props.country)
+        this.componentChangeCountry(this.props.country)
+    }
+
+    componentChangeCountry(country){
+        this.setState({...this.state, loading:true, error: null})
+        FoodApi.getMealsByCountry(country)
+        .then(data=>{
+            console.log(data)
+            setTimeout(()=>{
+                this.setState({
+                    ...this.state,
+                    loading: false,
+                    meals:[...data.meals]
+                })
+            }, 1000)
+        })
+        .catch((error)=>{
+            this.setState({
+                ...this.state, 
+                loading: false,
+                error: error.message
+            })
+        })
+    }
+        
+
+
+    render () {
+        return(
+            <div className="row">
+                {this.renderMeals()}
+            </div>
+        )
+    }
+}
